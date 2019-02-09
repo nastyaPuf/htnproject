d5839a9e9f90edea0b4295a76a6a5f00aeb14b89
nastyaPuf <47403051+nastyaPuf@users.noreply.github.com>
2019-02-10 00:06:44 +0300

    Add files via upload

---
+++ myproject/parser/domain_parser.py
@@ -0,0 +1,358 @@
+## important classes
+
+class Type:
+    def __init__(self, name, val):
+        self.name = name
+        self.val = val
+
+class Task:
+    def __init__(self, name, param, precond, effect):
+        self.name = name
+        self.param = param
+        self.precond = precond
+        self.effect = effect
+
+class Subtask:
+    def __init__(self, name, tasks):
+        self.name = name
+        self.tasks = tasks
+
+class Action:
+    def __init__(self, name, param, precond, effect):
+        self.name = name
+        self.param = param
+        self.precond = precond
+        self.effect = effect
+
+class Method:
+    def __init__(self, name, params, subtask, task, order):
+        self.name = name
+        self.task = task
+        self.params = params
+        self.subtask = subtask
+        self.order = order
+
+
+class Predicate:
+    def __init__(self, name, par):
+        self.name = name
+        self.par = par
+
+
+class Domain:
+    def __init__(selfself, name, requir, type, pred, action, task, method):
+        Domain.name = name
+        Domain.requir = requir
+        Domain.type = type
+        Domain.pred = pred
+        Domain.action = action
+        Domain.task = task
+        Domain.method = method
+
+class ParseError(Exception):
+    pass
+
+
+##function to read domain file
+def read_domain(Dom):
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    for i in range (len(text)):
+        text[i] = text[i].strip('(')
+        text[i] = text[i].strip(')')
+    return text
+
+def par_in_act(par):
+    s = []
+    par = par.replace(',', "")
+    par = par.replace(" ", "")
+    par = par.replace("'", "")
+    left = par.find('?')
+    while (1):
+        s1 = par.find('-', left)
+        right = par.find('?', s1)
+        if right == -1:
+            right = len(par) - 1
+        s.append(par[s1 + 1: right])
+        if right == len(par) - 1:
+            break
+        left = right
+    return s
+
+##parsing fuctions
+def parse_action(Dom):
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    act = 0
+    ef = 0
+    brac = 0
+    prec = 0
+    ef_act = []
+    prec_act = []
+    actions = []
+    act_name = ""
+    par_act = ""
+    for i in range(len(text)):
+        if text[i].find('(') != -1:
+            brac += 1
+        if text[i].find(')') != -1:
+            brac -= 1
+        if act == 1 and brac == 0:
+            Act = Action(act_name, par_act, prec_act, ef_act)
+            actions.append(Act)
+            act_name = ""
+            par_act = ""
+            ef_act = []
+            prec_act = []
+            act = 0
+            ef = 0
+            prec = 0
+        if text[i].find(':action') != -1:
+            text[i] = text[i].split()
+            act_name = text[i][1:]
+            act = 1
+            brac = 1
+            continue
+        if text[i].find(':param') != -1 and act == 1:
+            text[i] = text[i].replace("(", "")
+            text[i] = text[i].replace(")", "")
+            text[i] = text[i].split()
+            par_act = text[i][1:]
+            continue
+        if ef == 1 and text[i].find('and') == -1 and act == 1:
+            text[i] = text[i].replace("(", "")
+            text[i] = text[i].replace(")", "")
+            ef_act.append(text[i])
+        if text[i].find(':effect') != -1 and act == 1:
+            prec = 0
+            ef = 1
+        if prec == 1 and text[i].find('and') == -1 and act == 1:
+            text[i] = text[i].strip('(').strip(')')
+            prec_act.append(text[i])
+        if text[i].find(':precond') != -1 and act == 1:
+            prec = 1
+    return actions
+
+
+def parse_predic(Dom):
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    predic = list()
+    pred = 0
+    brac = 100
+    pr_name = ""
+    for i in range(len(text)):
+        if text[i].find(':predicates') != -1:
+            brac = 2
+            pred = 1
+        if text[i].find("(") != -1:
+            brac +=1
+            if pred == 1 and brac == 3:
+                pr_name = text[i][2:]
+                brac = 1
+        if text[i].find(")") != -1:
+            brac -=1
+            if pred == 1 and brac == 1:
+                predic.append(text[i][1:-1])
+        if (brac == 0):
+            break
+    return Predicate(pr_name, predic)
+
+
+def parse_task(Dom):
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    act = 0
+    ef = 0
+    brac = 0
+    prec = 0
+    ef_act = []
+    prec_act = []
+    actions = []
+    act_name = ""
+    par_act = ""
+    for i in range(len(text)):
+        if (text[i].find('method') != -1):
+            return actions
+        if text[i].find('(') != -1:
+            brac += 1
+        if text[i].find(')') != -1:
+            brac -= 1
+        if act == 1 and brac == 0:
+            task = Task(act_name, par_act, prec_act, ef_act)
+            actions.append(task)
+            act_name = ""
+            par_act = ""
+            ef_act = []
+            prec_act = []
+            act = 0
+            ef = 0
+            prec = 0
+        if text[i].find(':task') != -1:
+            text[i] = text[i].split()
+            act_name = text[i][1:]
+            act = 1
+            brac = 1
+            continue
+        if text[i].find(':param') != -1 and act == 1:
+            text[i] = text[i].replace("(", "")
+            text[i] = text[i].replace(")", "")
+            text[i] = text[i].split()
+            par_act = text[i][1:]
+            continue
+        if ef == 1 and text[i].find('and') == -1 and act == 1:
+            text[i] = text[i].replace("(", "")
+            text[i] = text[i].replace(")", "")
+            ef_act.append(text[i])
+        if text[i].find(':effect') != -1 and act == 1:
+            prec = 0
+            ef = 1
+        if prec == 1 and text[i].find('and') == -1 and act == 1:
+            text[i] = text[i].strip('(').strip(')')
+            prec_act.append(text[i])
+        if text[i].find(':precond') != -1 and act == 1:
+            prec = 1
+    return actions
+
+
+def parse_types(Dom):
+    s = ""
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    for i in range(len(text)):
+        if text[i].find(":types") != -1:
+            text[i] = text[i].strip(')').strip('(')
+            text[i] = text[i].split()
+            s = text[i][1:]
+    return s
+
+def parse_requirement(Dom):
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    for i in range(len(text)):
+        if (text[i].find(":requirements") != -1):
+            text[i] = text[i].strip('(').strip(')')
+            text[i] = text[i].split()
+            return text[i][1:]
+
+
+def parse_method(Dom):
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    meth_name = ""
+    meth_task = []
+    meth_subtask = []
+    meth_param = []
+    meth_order = []
+    methods = []
+    mth = 0
+    subt = 0
+    order = 0
+    for i in range(len(text)):
+        if (text[i].find('action') != -1):
+            return methods
+        if text[i].find(":method") != -1:
+            text[i] = text[i].strip(')').strip('(')
+            text[i] = text[i].split()
+            meth_name = text[i][1:]
+            mth = 1
+        elif (text[i] == '))'):
+            meth_param = par_in_act(meth_param)
+            m = Method(meth_name, meth_param, meth_subtask, meth_task, meth_order)
+            methods.append(m)
+            meth_name = ""
+            meth_task = ""
+            meth_subtask = []
+            meth_param = ""
+            meth_order = []
+            mth = 0
+            order = 0
+        elif (text[i].find(':task') != -1 and mth == 1):
+            text[i] = text[i].strip(')').strip('(')
+            text[i].replace("(", "")
+            text[i] = text[i].split()
+            meth_task = text[i][1:]
+        elif (text[i].find(':subtask') != -1 and mth == 1):
+            subt = 1
+        elif (text[i].find(':param') != -1 and mth == 1):
+            text[i].strip(')').strip('(')
+            text[i].split()
+            meth_param = text[i][1:]
+        elif (text[i].find(':ordering') != -1):
+            order = 1
+            subt = 0
+        elif (subt == 1 and mth == 1 and order == 0):
+            firpar = text[i].find('(')
+            secpar = text[i].find('(', firpar + 1)
+            s = Subtask
+            s.name = text[i][firpar:secpar - 1]
+            s.tasks = text[i][secpar + 1:]
+            meth_subtask.append(s)
+        elif (order == 1):
+            text[i].strip(')').strip('(')
+            meth_order.append(text[i])
+
+
+
+
+#parse domain file and answer
+def domain_parser(Dom):
+    x = 5
+    y = 5
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    text[0] = text[0].strip(')').strip('(')
+    text[0] = text[0].split()
+    D = Domain(text[0][2:], parse_requirement(Dom), parse_types(Dom), parse_predic(Dom), parse_action(Dom), parse_task(Dom), parse_method(Dom))
+    print("domain name - ", D.name)
+    print("___________________________________")
+    print("requirements - ", parse_requirement(Dom))
+    print("___________________________________")
+    print("types - ", parse_types(Dom))
+    print("___________________________________")
+    print("predicates:")
+    for i in D.pred.par:
+        print(i)
+    print("___________________________________")
+    print("actions: ")
+    for act in D.action:
+        print("name - ", act.name)
+        print("parametres - ", act.param)
+        print("preconditions - ", act.precond)
+        print("effects - ", act.effect)
+        print ("*******")
+    print()
+    print("tasks: ")
+    for tsk in D.task:
+        print("name - ", tsk.name)
+        print("parametres - ", tsk.param)
+        print("preconditions - ", tsk.precond)
+        print("effects - ", tsk.effect)
+        print("++++++")
+    print()
+    print()
+    print("methods: ")
+    for mth in D.method:
+        print("name - ", mth.name)
+        print("task - ", mth.task)
+        print("parametres - ", mth.params)
+        print("subtask:")
+        for s in mth.subtask:
+            print(s.name, " -", s.tasks)
+        if (mth.order != []):
+            print("ordering:")
+            print(*mth.order)
+        print('++++++')
+        print()
+
+##парсит Домен, не печатая его
+def parse_domain(Dom):
+    with open(Dom) as file:
+        text = [row.strip() for row in file]
+    text[0] = text[0].strip(')').strip('(')
+    text[0] = text[0].split()
+    D = Domain(text[0][2:], parse_requirement(Dom), parse_types(Dom), parse_predic(Dom), parse_action(Dom), parse_task(Dom), parse_method(Dom))
+    return D
+
+
+
---
+++ myproject/parser/problem_parser.py
@@ -0,0 +1,78 @@
+def del_pr(str):
+    str = str.strip(')').strip('(')
+    str = str.split()
+    return str
+
+class Problem:
+    def __init__(self, name, domain, obj, init, goal):
+        Problem.name = name
+        Problem.domain = domain
+        Problem.obj = obj
+        Problem.init = init
+        Problem.goal = goal
+
+
+def parse_problem(Prob):
+    with open(Prob) as file:
+        text = [row.strip() for row in file]
+    pr_name = ""
+    domain = ""
+    obj = dict()
+    ob = 0
+    pr_init = []
+    goal = 0
+    pr_goal = []
+    ini = 0
+    for i in range (len(text)):
+        if (text[i].find(':problem') != -1):
+            text[i] = del_pr(text[i])
+            pr_name = text[i][1:]
+        elif (text[i].find(':domain') != -1):
+            text[i] = del_pr(text[i])
+            domain = text[i][1:]
+        elif (text[i].find(':objects') != -1):
+            ob = 1
+        elif (text[i].find(':init') != -1):
+            ob = 0
+            ini = 1
+        elif (text[i].find(':goal') != -1):
+            ini = 0
+            goal = 1
+        elif (ob == 1 and text[i].find(')') == -1):
+            text[i].replace(' ', '')
+            text[i] = text[i].strip(')').strip('(')
+            obj[text[i][text[i].find('-') + 2:]] = text[i][:text[i].find('-') - 1]
+        elif (ini == 1 and goal != 1):
+            text[i] = del_pr(text[i])
+            if (len(text[i]) != 0):
+                pr_init.append(text[i])
+        elif (goal == 1 and i < len(text)):
+            text[i] = del_pr(text[i])
+            pr_goal.append(text[i])
+    pr = Problem(pr_name, domain, obj, pr_init, pr_goal)
+    return pr
+
+
+def print_parsing(Prob):
+    problem = parse_problem(Prob)
+    print("name - ", problem.name)
+    print ('______________________')
+    print("domain -", problem.domain)
+    print('______________________')
+    print("objects:")
+    for objects in problem.obj:
+        print(objects, "-", problem.obj[objects])
+    print('______________________')
+    print("initial state:")
+    for state in problem.init:
+        print(state)
+    print('______________________')
+    print("goal:")
+    for goals in problem.goal:
+        print(goals)
+
+
+
+
+
+
