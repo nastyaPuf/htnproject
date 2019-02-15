83e9fc613ad205f1d63eec4c72fb8feef8cb9a57
nastyaPuf <47403051+nastyaPuf@users.noreply.github.com>
2019-02-15 15:34:32 +0300

    Update main_file.py

--- myproject/parser/main_file.py
+++ myproject/parser/main_file.py
@@ -5,7 +5,7 @@ def main(argv):
     #dp.parse_domain(argv[1])
     #pp.parse_problem(argv[2])
     g = gr.Grounder()
-    gr.everything(argv[1], argv[2])
+    g.everything(argv[1], argv[2])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
